86673545d1488476b13249bac9dc8593e717b90d
Geoff Schwartz <eieio@pixelsoup.com.au>
2024-10-22 19:57:03 +1100

    FIrst commit

---
+++ js/hello-button.js
@@ -0,0 +1,10 @@
+class HelloButton extends HTMLButtonElement {
+
+  constructor() {
+
+    super()
+    this.addEventListener('click', () => alert("Hello!"))
+  }
+}
+
+customElements.define('hello-button', HelloButton, {extends: 'button'})
